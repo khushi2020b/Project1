3fd35d7a2d2f223df3fecc49073df66f2f98c7a5
khushi2020b <khushi2020b@gmail.com>
2022-07-01 17:23:40 +0530

    first commit

---
+++ 1st.py
@@ -0,0 +1,20 @@
+
+# from numpy import random
+
+# x = random.randint(100)
+# print(x)
+
+# arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+# dim, ndim
+# arr = np.array([1,2,3,4],ndmin = 5)
+# print(arr)
+
+import matplotlib.pyplot as pl
+import numpy as np
+
+x = np.array([0,6])
+y = np.array([0,200])
+
+pl.plot(x,y)
+pl.show()
+
